05b93ee316066d77c4c2c12cf175e5b0e8e999b0
GurjotChohan <chohangurjot7@gmail.com>
2023-02-12 04:56:13 -0600

    final commit

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import { mockTransactions } from "../../data/mockData";
-import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import Header from "../../components/Header";
 
 const Dashboard = () => {
@@ -46,6 +46,9 @@ const Dashboard = () => {
           gridColumn="span 8"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
+          style={{ 
+            backgroundImage: `url("https://images.unsplash.com/photo-1585822780212-e96a20643ac4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhlcnRmb3Jkc2hpcmV8ZW58MHx8MHx8&w=1000&q=80")` 
+          }}
         >
           <Box
             mt="25px"
@@ -56,7 +59,7 @@ const Dashboard = () => {
           >
             <Box >
               <Typography
-                variant="h3"
+                variant="h1"
                 fontWeight="600"
                 color={colors.grey[100]}
               >
@@ -65,10 +68,16 @@ const Dashboard = () => {
               <Typography
                 variant="h3"
                 fontWeight="bold"
-                color={colors.greenAccent[500]}
+                color={colors.greenAccent[700]}
               >
                 RICHARDSON, TEXAS
               </Typography>
+              <h3>The Weather in your area is perfect! It is safe to drive</h3>
+
+              <h1 style={{ 
+            fontSize: "80px"}}>
+                35° - 70°
+                </h1>
             </Box>
             <Box>
 
@@ -158,14 +167,30 @@ const Dashboard = () => {
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
+          style={{
+          backgroundImage: "url(" + "https://i.pinimg.com/originals/0c/ff/42/0cff425ac8447e4d7be40b82a592cbab.jpg" + ")" ,
+          backgroundSize: '1200px 2000px',
+          }}
         >
           <Typography
-            variant="h5"
+            variant="h2"
             fontWeight="600"
-            sx={{ padding: "30px 30px 0 30px" }}
+            sx={{ padding: "30px 30px 30px 30px" }}
           >
-            Sales Quantity
+            Your Car Information
           </Typography>
+          <Typography 
+            variant="h4"
+            color={colors.greenAccent[900]}
+            sx={{ padding: "0px 40px 30px 30px" }}
+            >
+             <div style = {{ padding: "0px 35px 10px 30px" }}>Car Model: Carmary 2023 </div>
+             <div style = {{ padding: "0px 35px 10px 30px" }}>VIN: 1HGBH41JXMN109182 </div>
+             <div style = {{ padding: "0px 35px 10px 30px" }}>Last Service: 02.12.23 </div>
+             <div style = {{ padding: "0px 35px 10px 30px" }}>Last oil change: 02.12.23 </div>
+             <div style = {{ padding: "0px 35px 10px 30px" }}>Overall Health: Perfect! </div>
+          </Typography>
+
           <Box height="250px" mt="-20px">
           </Box>
         </Box>
@@ -176,12 +201,74 @@ const Dashboard = () => {
           padding="30px"
         >
           <Typography
-            variant="h5"
+            variant="h2"
             fontWeight="600"
             sx={{ marginBottom: "15px" }}
           >
-            Geography Based Traffic
+              <IconButton>
+                <AccountBoxIcon
+                  sx={{ fontSize: "50px", color: colors.greenAccent[500]}}
+                />
+              </IconButton>
+            Profile
           </Typography>
+          <div>
+          <span>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "20px 20px",
+            }}
+          >
+            <div> &nbsp; Driver History &nbsp;</div>
+          </Button>
+          </span>
+           <span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</span>
+           <span>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "20px 20px",
+            }}
+          >
+            <div> &nbsp; Seat Positioning &nbsp;</div>
+          </Button>
+          </span>
+          </div>
+          <div style = {{ padding: "0px 0px 40px 0px" }}>{'\n'}</div>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "20px 20px",
+            }}
+          >
+             <div> &nbsp; Mirror Adujstments &nbsp;</div>
+          </Button>
+
+          <span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</span>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "20px 20px",
+            }}
+          >
+             <div>&nbsp; Maps settings &nbsp;</div>
+          </Button>
+
+
+
           <Box height="200px">
           </Box>
         </Box>
